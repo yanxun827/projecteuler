61389697e3fd8abd4de85f0f65988bb6ffc15735
Yan Xun Ng <yanxun827@gmail.com>
2016-03-27 20:02:15 +0100

    add probelm 9, 11

--- Python/007.py
+++ Python/007.py
@@ -10,19 +10,19 @@ What is the 10001st prime number?
 
 
 def findPrime(position):
-	primes = []
-	number = 2
-	while len(primes) < position:
-		isprime = True
-		for i in primes:
-			if number%i == 0:
-				isprime = False
-				break
-		if isprime == True:
-			primes.append(number)
-		number += 1
-	return primes[-1]
+    primes = []
+    number = 2
+    while len(primes) < position:
+        isprime = True
+        for i in primes:
+            if number % i == 0:
+                isprime = False
+                break
+        if isprime == True:
+            primes.append(number)
+        number += 1
+    return primes[-1]
 
 if __name__ == "__main__":
-	position = 10001
-	print(findPrime(position))
+    position = 10001
+    print(findPrime(position))
